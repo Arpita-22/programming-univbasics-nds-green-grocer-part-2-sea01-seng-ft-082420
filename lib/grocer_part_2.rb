fabe183f823abce791ae03af363f50506c3524b6
Arpita Dutta <duttaarpita22@gmail.com>
2020-08-11 03:52:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -44,10 +44,10 @@ def checkout(cart, coupons)
   data = apply_clearance(coupon)
   grand_total = 0
   data.each do |data_item|
-    grand_total = data_item[:price]*data_item[:count]
+    grand_total = data_item[:price] * data_item[:count]
   end
   if grand_total > 100
-    grand_total = (grand_total - grand_total*0.1).round(2)
+    grand_total = (grand_total - grand_total * 0.1).round(2)
   end
   grand_total
 end
